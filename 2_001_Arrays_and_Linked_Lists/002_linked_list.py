50618c8f58f95c991bf900e3e4e33ab871016b8f
Ludovico Pinzari <ludovico.pinzari@gmail.com>
2020-02-14 15:09:00 +0100

    feat: implement linked list

---
+++ 2_001_Arrays_and_Linked_Lists/002_linked_list.py
@@ -0,0 +1,70 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+head = Node(2)
+head.next = Node(1)
+head.next.next = Node(4)
+head.next.next.next = Node(3)
+head.next.next.next.next = Node(5)
+
+# Let's print the values of all the nodes to check if it worked. If you successfully created (and linked) all the nodes, the following should print out 2, 1, 4, 3, 5:
+
+print(head.value)
+print(head.next.value)
+print(head.next.next.value)
+print(head.next.next.next.value)
+print(head.next.next.next.next.value)
+
+def create_linked_list(input_list):
+    """
+    Function to create a linked list
+    @param input_list: a list of integers
+    @return: head node of the linked list
+    """
+    head = None
+    tail = None
+
+    for value in input_list:
+
+        if head is None:
+            head = Node(value)
+            tail = head # when we only have 1 node, head and tail refer to the same node
+        else:
+            tail.next = Node(value) # attach the new node to the `next` of tail
+            tail = tail.next # update the tail
+
+    return head
+
+
+### Test Code
+def test_function(input_list, head):
+    try:
+        if len(input_list) == 0:
+            if head is not None:
+                print("Fail")
+                return
+        for value in input_list:
+            if head.value != value:
+                print("Fail")
+                return
+            else:
+                head = head.next
+        print("Pass")
+    except Exception as e:
+        print("Fail: "  + e)
+
+
+
+input_list = [1, 2, 3, 4, 5, 6]
+head = create_linked_list(input_list)
+test_function(input_list, head)
+
+input_list = [1]
+head = create_linked_list(input_list)
+test_function(input_list, head)
+
+input_list = []
+head = create_linked_list(input_list)
+test_function(input_list, head)
